32ad27f259198c4f584338cdc688f75fd1603952
SkySwordSaint26 <mistry.shubham2606@gmail.com>
2026-05-13 17:11:05 +0530

    Added relations for drizzle

---
+++ server/src/db/relations.js
@@ -0,0 +1,65 @@
+import { relations } from "drizzle-orm";
+
+import {
+  Users,
+  Categories,
+  Transactions,
+  Budgets,
+  Insights,
+} from "./schema";
+
+// User Realtions
+export const userRelations = relations(Users, ({many})=>({
+    transactions: many(Transactions),
+    budgets: many(Budgets),
+    insights: many(Insights),
+}))
+
+export const categoryRelations = relations(
+    Categories, ({many}) => ({
+        transactions: many(Transactions),
+        budgets: many(Budgets),
+    })
+);
+
+
+// one-to-one relation
+export const transactionRealtions = relations(
+    Transactions, 
+    ({one}) => ({
+        user: one(Users, {
+            fields: [Transactions.userId],
+            references: [Users.userId],
+        }),
+
+        category: one(Categories, {
+            fields: [Transactions.categoryId],
+            references: [Categories.categoryId],
+        }),
+    })
+);
+
+export const budgetRelations = relations(
+    Budgets,
+    ({one}) => ({
+        user: one(Users,{
+            fields: [Budgets.userId],
+            references: [Users.userId],
+        }),
+
+        category: one(Categories, {
+            fields: [Budgets.categoryId],
+            references: [Categories.categoryId],
+        }),
+    })
+);
+
+export const insightsRelations = relations(
+  Insights,
+  ({ one }) => ({
+    user: one(Users, {
+      fields: [Insights.userId],
+      references: [Users.userId],
+    }),
+  })
+);
